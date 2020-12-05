8fa61ae54d204a02b49ba4d4f0dac0fdb0d0f28b
Troll1r <kiralpa1503@gmail.com>
2020-12-05 12:03:16 +0400

    Initial commit

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public GameObject world1;
+    public GameObject world2;
+    bool Active = true;
+    void Start()
+    {
+        world2.SetActive(true);
+        world1.SetActive(false);
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+    public void ActiveWorld() 
+    {
+        if (Active == true)
+        {
+            world2.SetActive(true);
+            world1.SetActive(false);
+            Active = false;
+        }
+        else
+        {
+            world2.SetActive(false);
+            world1.SetActive(true);
+
+            Active = true;
+        }
+        }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float WalkSpeed;
+    public float JumpForce = 5;
+    float Move;
+
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+    private bool isGrounded;
+
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Move, 0, 0);
+
+    }
+
+    public void MoveRight() 
+    {
+        Move = WalkSpeed;
+    }
+    public void MoveLeft()
+    {
+        Move = -WalkSpeed;
+    }
+    public void Jump() 
+    {
+        if(isGrounded)
+        _rigidbody.AddForce(new Vector2(0, JumpForce),ForceMode2D.Impulse);
+    }
+    public void Stop()
+    {
+        Move = 0;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        isGrounded = false;
+    }
+
+}
